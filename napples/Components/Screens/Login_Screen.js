f569d2da7ddbf30eb268985a41f8c6763c47adf7
Dxrxzio <62361596+Dxrxzio@users.noreply.github.com>
2021-02-16 19:01:21 -0500

    Added Login Screen.

---
+++ napples/Components/Screens/Login_Screen.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+
+export  default class Login_Screen extends  React.Component{
+
+}
